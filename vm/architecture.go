cfa5f79c8876c0b2edc991bf6cdffc92ab08bc93
aewens <email@aewens.com>
2021-01-10 04:51:17 +0000

    Added AND, NOT, and BR operations

--- vm/architecture.go
+++ vm/architecture.go
@@ -86,6 +86,12 @@ func (self *LC3) readMemoryIndirect(indirectLocation int) int {
 	return self.readMemory(location)
 }
 
+func (self *LC3) readMemoryNext() int {
+	location := self.Registers[R_PC]
+	self.checkLocation(location)
+	return self.Memory[location]
+}
+
 func (self *LC3) signExtend(value int, bits int) int {
 	// Check if value is negative / if first bit is a 1 using 2's complement
 	if (value>>(bits-1))&1 == 1 {
@@ -112,8 +118,7 @@ func (self *LC3) updateFlags(index int) {
 		return
 	}
 
-	self.Registers[R_COND] = FL_P
-}
+	self.Registers[R_COND] = FL_P }
 
 /*
 	Terminology:
@@ -123,6 +128,24 @@ func (self *LC3) updateFlags(index int) {
 	* IM5: 5-bit Immediate Value; int4 (-16 to 15)
 	* O09: Offset PC by 9; int9 (-256 to 255)
 */
+
+func (self *LC3) Branch(instruction int) {
+	/*
+		Encoding:
+		FEDC|B|A|9|876543210
+			* Bits F-C: 0000 (opcode)
+			* Bits _-B: N
+			* Bits _-A: Z
+			* Bits _-9: P
+			* Bits 8-0: O09
+	*/
+
+	offset := self.signExtend(instruction&0b111111111, 9)
+	cond := (instruction >> 0x9) & 0b111
+	if (cond & self.Registers[R_COND]) == 1 {
+		self.Advance(offset)
+	}
+}
 
 func (self *LC3) Add(instruction int) {
 	/*
@@ -160,6 +183,64 @@ func (self *LC3) Add(instruction int) {
 	self.updateFlags(dr)
 }
 
+func (self *LC3) And(instruction int) {
+	/*
+		Encoding:
+		a. Register Mode - FEDC|BA9|876|5|43|210
+			* Bits F-C: 0101 (opcode)
+			* Bits B-9: DR
+			* Bits 8-6: SR1
+			* Bits _-5: 0 (Register Mode)
+			* Bits 4-3: 00 (unused)
+			* Bits 2-0: SR2
+		b. Immediate Mode - FEDC|BA9|876|5|43210
+			* Bits F-C: 0101 (opcode)
+			* Bits B-9: DR
+			* Bits 8-6: SR1
+			* Bits _-5: 1 (Immediate Mode)
+			* Bits 4-0: IM5
+	*/
+
+	dr := (instruction >> 0x9) & 0b111
+	self.checkRegister(dr)
+
+	sr1 := (instruction >> 0x6) & 0b111
+	self.checkRegister(sr1)
+
+	mode := (instruction >> 0x5) & 0b1
+	if mode == 1 {
+		im5 := self.signExtend(instruction&0b11111, 5)
+		self.Registers[dr] = self.Registers[sr1] & im5
+	} else {
+		sr2 := instruction & 0b111
+		self.Registers[dr] = self.Registers[sr1] & self.Registers[sr2]
+	}
+
+	self.updateFlags(dr)
+}
+
+func (self *LC3) Not(instruction int) {
+	/*
+		Encoding:
+		FEDC|BA9|876|5|43210
+			* Bits F-C: 1001 (opcode)
+			* Bits B-9: DR
+			* Bits 8-6: SR1
+			* Bits _-5: 1 (literal, unused)
+			* Bits 4-0: 11111 (literal, unused)
+	*/
+
+	dr := (instruction >> 0x9) & 0b111
+	self.checkRegister(dr)
+
+	sr1 := (instruction >> 0x6) & 0b111
+	self.checkRegister(sr1)
+
+	// Bitwise not on integer
+	self.Registers[dr] = int(^byte(self.Registers[sr1]))
+	self.updateFlags(dr)
+}
+
 func (self *LC3) LoadIndirect(instruction int) {
 	/*
 		Encoding:
@@ -179,15 +260,18 @@ func (self *LC3) LoadIndirect(instruction int) {
 	self.updateFlags(dr)
 }
 
+func (self *LC3) Advance(offset int) {
+	counter := self.Registers[R_PC]
+	self.Registers[R_PC] = counter + offset
+}
+
 func (self *LC3) Step() {
 	if self.Halted {
 		return
 	}
 
-	counter := self.Registers[R_PC]
-	self.Registers[R_PC] = counter + 1
-
-	instruction := self.readMemory(counter)
+	instruction := self.readMemoryNext()
+	self.Advance(1)
 	op := instruction >> 12
 	/*
 		Okay, so instructions in LC-3 are 16 bits wide used as follows:
@@ -202,9 +286,12 @@ func (self *LC3) Step() {
 	switch op {
 	case OP_ADD:
 		self.Add(instruction)
-	//case OP_AND:
-	//case OP_NOT:
-	//case OP_BR:
+	case OP_AND:
+		self.And(instruction)
+	case OP_NOT:
+		self.Not(instruction)
+	case OP_BR:
+		self.Branch(instruction)
 	//case OP_JMP:
 	//case OP_JSR:
 	//case OP_LD:
--- vm/architecture_test.go
+++ vm/architecture_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
+func TestAddRegister(t *testing.T) {
 	computer := New()
 	computer.Registers[R_0] = 1
 	computer.Registers[R_1] = 2
@@ -13,6 +13,19 @@ func TestAdd(t *testing.T) {
 	value := computer.Registers[R_2]
 
 	expect := 3
+	if value != expect {
+		t.Fatalf("Expected %d: %d", expect, value)
+	}
+}
+
+func TestAddImmediate(t *testing.T) {
+	computer := New()
+	computer.Registers[R_0] = 1
+	computer.Memory[PC_START] = 0b0001010000111110
+	computer.Step()
+	value := computer.Registers[R_2]
+
+	expect := 0xFFFF
 	if value != expect {
 		t.Fatalf("Expected %d: %d", expect, value)
 	}
@@ -27,6 +40,97 @@ func TestLoadIndirect(t *testing.T) {
 	value := computer.Registers[R_1]
 
 	expect := 2
+	if value != expect {
+		t.Fatalf("Expected %d: %d", expect, value)
+	}
+}
+
+func TestAndRegister(t *testing.T) {
+	computer := New()
+	computer.Registers[R_0] = 0xFF
+	computer.Registers[R_1] = 0xF0
+	computer.Memory[PC_START] = 0b0101010000000001
+	computer.Step()
+	value := computer.Registers[R_2]
+
+	expect := 0xF0
+	if value != expect {
+		t.Fatalf("Expected %d: %d", expect, value)
+	}
+}
+
+func TestAndImmediate(t *testing.T) {
+	computer := New()
+	computer.Registers[R_0] = 0xFF
+	computer.Memory[PC_START] = 0b0101010000101111
+	computer.Step()
+	value := computer.Registers[R_2]
+
+	expect := 0xF
+	if value != expect {
+		t.Fatalf("Expected %d: %d", expect, value)
+	}
+}
+
+func TestNot(t *testing.T) {
+	computer := New()
+	computer.Registers[R_0] = 0xF0
+	computer.Memory[PC_START] = 0b1001001000111111
+	computer.Step()
+	value := computer.Registers[R_1]
+
+	expect := 0xF
+	if value != expect {
+		t.Fatalf("Expected %d: %d", expect, value)
+	}
+}
+
+func TestBranchN(t *testing.T) {
+	computer := New()
+	computer.Registers[R_0] = 0xFFFE
+	computer.Registers[R_1] = 1
+	computer.Memory[PC_START] = 0b0001010000000001
+	computer.Memory[PC_START+1] = 0b0000100000000001
+	computer.Memory[PC_START+2] = 3
+	computer.Step()
+	computer.Step()
+	value := computer.readMemoryNext()
+
+	expect := 3
+	if value != expect {
+		t.Fatalf("Expected %d: %d", expect, value)
+	}
+}
+
+func TestBranchZ(t *testing.T) {
+	computer := New()
+	computer.Registers[R_0] = 0
+	computer.Registers[R_1] = 0
+	computer.Memory[PC_START] = 0b0001010000000001
+	computer.Memory[PC_START+1] = 0b0000100000000001
+	computer.Memory[PC_START+2] = 3
+	computer.Step()
+	computer.Step()
+	value := computer.readMemoryNext()
+
+	expect := 3
+	if value != expect {
+		t.Fatalf("Expected %d: %d", expect, value)
+	}
+}
+
+func TestBranchP(t *testing.T) {
+	computer := New()
+	computer.Registers[R_0] = 1
+	computer.Registers[R_1] = 1
+	computer.Memory[PC_START] = 0b0001010000000001
+	computer.Memory[PC_START+1] = 0b0000100000000001
+	computer.Memory[PC_START+2] = 3
+	computer.Step()
+	computer.Step()
+	value := computer.readMemoryNext()
+
+	expect := 3
 	if value != expect {
 		t.Fatalf("Expected %d: %d", expect, value)
 	}
